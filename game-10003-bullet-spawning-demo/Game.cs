a0356af13acef71733453b694eb9204b4f5e793c
ewand <ewand@EWANS_LAPTOP>
2025-11-04 18:17:19 -0500

    i like cheese.

---
+++ game-10003-bullet-spawning-demo/Game.cs
@@ -0,0 +1,20 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        public void Setup()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+
+}
