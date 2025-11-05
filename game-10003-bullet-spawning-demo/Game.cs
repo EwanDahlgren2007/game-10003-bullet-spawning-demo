00164a0b000b4c66828b8fec37ccb68797a79497
ewand <ewand@EWANS_LAPTOP>
2025-11-04 19:00:44 -0500

    poop.

--- game-10003-bullet-spawning-demo/Game.cs
+++ game-10003-bullet-spawning-demo/Game.cs
@@ -7,13 +7,53 @@ namespace MohawkGame2D
 {
     public class Game
     {
+        Bullet[] bullets = new Bullet[100];
+        int bulletIndex = 0;
+
+        int ammo = 100;
         public void Setup()
         {
-
+            Window.SetTitle("Bullet Spawning");
+            Window.SetSize(800, 600);
+            Window.TargetFPS = 240;
         }
         public void Update()
         {
+            Window.ClearBackground(Color.White);
 
+            if (Input.IsMouseButtonPressed(MouseInput.Left)) SpawnBullet();
+
+            for (int i = 0; i < bullets.Length; i++)
+            // call update on all bullets
+            {
+                if (bullets[i] != null) bullets[i].Update();
+                // skip the bullet if it hasn't been spawned yet
+            }
+
+            Text.Draw($"Ammo: {ammo}", new Vector2(10, 10));
+
+        }
+        void SpawnBullet()
+        {
+            // don't spawn the bullets if you are out of ammo
+            if (bullets[bulletIndex] != null) return;
+
+            // when mouse button is pressed, spawn a bullet!
+            Bullet bullet = new Bullet();
+
+            Vector2 centerScreen = Window.Size / 2.0f;
+
+            bullet.position = Window.Size / 2.0f;
+
+            Vector2 centerToMouse = Input.GetMousePosition() - centerScreen;
+            bullet.velocity = Vector2.Normalize(centerToMouse);
+
+            bullets[bulletIndex] = bullet;
+            bulletIndex++;
+
+            if (bulletIndex >= bullets.Length) bulletIndex = 0;
+
+            ammo -= 1;
         }
     }
 
